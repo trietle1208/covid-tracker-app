c1fc36b9ffda36f4456543ef09089298b7c586ad
TrietLe <lmtriet1208@gmail.com>
2022-03-22 21:26:23 +0700

    first commit

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <select @change="onChange()" name="" id="" v-model="selected" class="form-select mt-1 block w-full border p-3 rounded">
+      <option value="0">Select Country</option>
+      <option v-for="country in countries" :value="country.ID">
+          {{ country.Country }}
+      </option>
+  </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    data() {
+        return {
+            selected: 0,
+        }
+    },
+    methods: {
+        onChange(){
+            const country = this.countries.find((item) => item.ID === this.selected);
+            this.$emit('get-country', country)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main v-if="!loading">
+    <data-title :text="title" :dataDate="dataDate"></data-title>
+
+    <data-boxes :status="status"></data-boxes>
+
+    <country-select @get-country="getCountryData" :countries="countries"></country-select>
+
+    <button @click="onClear" v-if="status.Country" class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">
+      Clear Country
+    </button>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetch Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import DataTitle from "@/components/DataTitle.vue";
+import DataBoxes from "@/components/DataBoxes.vue";
+import CountrySelect from "@/components/CountrySelect.vue";
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      status: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+    getCountryData(country) {
+      this.status = country;
+      this.title = country.Country;
+    },
+    async onClear() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = 'Global';
+      this.status = data.Global;
+      this.loading = false;
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.status = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
   },
 };
 </script>
